fb3ee759f05d7febcde6591f275e318efbe74f11
Roland Huß <roland@cpan.org>
2009-06-28 11:38:31 +0000

    Import of ROLAND/jmx4perl-0.20 from CPAN.
    gitpan-cpan-distribution: jmx4perl
    gitpan-cpan-version:      0.20
    gitpan-cpan-path:         ROLAND/jmx4perl-0.20.tar.gz
    gitpan-cpan-author:       ROLAND
    gitpan-cpan-maturity:     released

--- it/it.pl
+++ it/it.pl
@@ -14,7 +14,7 @@ GetOptions("dir=s" => \$dir,
            "user=s" => \$user,
            "password=s" => \$password,
            "product=s" => \$product);
-die "No gateway url given.\n" unless $gateway_url;
+die "No gateway url given. Please use option '--url' for pointing to the server with the agent installed\n" unless $gateway_url;
 
 my @testfiles;
 if (@ARGV) {
@@ -33,7 +33,7 @@ my $harness = new TAP::Harness
     color => 1,
     merge => 1,
     jobs => 1,
-    lib => [ "$FindBin::Bin/../lib", "$FindBin::Bin/ilib" ]
+    lib => [ "$FindBin::Bin/../lib", "$FindBin::Bin/../t/lib" ]
    });
 
 $ENV{JMX4PERL_GATEWAY} = $gateway_url;
