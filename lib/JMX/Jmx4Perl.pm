4f022442cb710960cce9a15d08a7b1388d28eec3
Roland Huß <roland@cpan.org>
2009-08-12 19:27:26 +0000

    Import of ROLAND/jmx4perl-0.35_3 from CPAN.
    gitpan-cpan-distribution: jmx4perl
    gitpan-cpan-version:      0.35_3
    gitpan-cpan-path:         ROLAND/jmx4perl-0.35_3.tar.gz
    gitpan-cpan-author:       ROLAND
    gitpan-cpan-maturity:     developer

--- lib/JMX/Jmx4Perl.pm
+++ lib/JMX/Jmx4Perl.pm
@@ -101,7 +101,7 @@ use vars qw($VERSION $HANDLER_BASE_PACKAGE @PRODUCT_HANDLER_ORDERING);
 use Data::Dumper;
 use Module::Find;
 
-$VERSION = "0.35_2";
+$VERSION = "0.35_3";
 
 my $REGISTRY = {
                 # Agent based
@@ -350,7 +350,7 @@ sub search {
 
     return undef if $response->status eq "404"; # nothing found
     if ($response->is_error) {
-        croak "Error searching for $pattern: ",$response->error_text;
+        die "Error searching for $pattern: ",$response->error_text,Dumper($response);
     }
     return $response->value;    
 }
