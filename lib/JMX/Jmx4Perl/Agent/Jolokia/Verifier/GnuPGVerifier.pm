f3fbcb46f5bfeb84d3c5ad583e44af20dcc359cf
Roland Huß <roland@cpan.org>
2011-04-08 06:42:25 +0000

    Import of ROLAND/jmx4perl-0.90_5 from CPAN.
    gitpan-cpan-distribution: jmx4perl
    gitpan-cpan-version:      0.90_5
    gitpan-cpan-path:         ROLAND/jmx4perl-0.90_5.tar.gz
    gitpan-cpan-author:       ROLAND
    gitpan-cpan-maturity:     developer

--- lib/JMX/Jmx4Perl/Agent/Jolokia/Verifier/GnuPGVerifier.pm
+++ lib/JMX/Jmx4Perl/Agent/Jolokia/Verifier/GnuPGVerifier.pm
@@ -2,6 +2,7 @@
 
 package JMX::Jmx4Perl::Agent::Jolokia::Verifier::GnuPGVerifier;
 
+use JMX::Jmx4Perl::Agent::Jolokia::Verifier::PGPKey;
 use Module::Find;
 use Data::Dumper;
 use File::Temp qw/tempfile/;
@@ -58,7 +59,7 @@ sub verify {
         push @cmd, $signature_path,$args{path};
         my $cmd = join ' ', @cmd;
         my $output = `$cmd`;
-        if ($output =~ /public key/i) {
+        if ($output =~ /public\s*key/i) {
             # Import key and retry
             $self->_import_key(\%args);
             $output = `$cmd`;
@@ -97,20 +98,17 @@ sub _import_key {
 
     my $gpg = $self->{gpg};
     my $log = $args->{logger};
-    my $fh = \*{JMX::Jmx4Perl::Agent::Jolokia::Verifier::GnuPGVerifier::DATA};    
-    my $key =  join "",<$fh>;
-    close $fh;
-    my $key_path = $self->_store_tempfile($key);
+    my $key_path = $self->_store_tempfile($JMX::Jmx4Perl::Agent::Jolokia::Verifier::PGPKey::KEY);
 
     my @cmd = ($gpg,qw(--import --verbose --batch --no-tty  --logger-fd=1),$key_path);
     my $cmd = join ' ', @cmd;
-    my $output = `$cmd`;
+    my $output = `$cmd 2>&1`;
     if ($?) {
         $log->error("Cannot add public PGP used for verification to local keystore: $output");
         die "\n";
     } else {
-        $log->info($output);
-        my $info = $1 if $output =~ /([\dA-F]{8}.*)$/mi;
+        #$log->info($output);
+        my $info = $1 if $output =~ /([\dA-F]{8}.*import.*)$/mi;
         $log->info($info ? $info : "Added jmx4perl key");
     }
     unlink $key_path;
@@ -127,7 +125,7 @@ sub _gpg_version {
             die "Cannot find gpg or gpg2: $out\n";
         }
     }
-    $out =~ /GnuPG.*?(\S+)\s*$/m or die "Cannot execute gpg: $out";
+    $out =~ /GnuPG.*?(\S+)\s*$/m;
     return ($gpg,$1);
 }
 
@@ -167,31 +165,3 @@ further details.
 roland@cpan.org
 
 =cut
-
-
-__DATA__
------BEGIN PGP PUBLIC KEY BLOCK-----
-Version: GnuPG v1.4.10 (GNU/Linux)
-
-mQCNAzpoBEMAAAEEAMdDw9V+zMCjJI6Icjv+Z+s5mepNJ+tH848PVOfZohfDoEZx
-pthbKW+U0EgFVtV8EE9iWDQOh68U3BvEaOvk+99YoahRRACuII1Y+Q445UaNV/Tn
-hCGmofWITYY8Tbz6dcYnWsWMQ5XByM4aMwucM8pUARomkrrM9kKyJpPvEBFlAAUR
-tCFSb2xhbmQgSHVzcyA8cm9sYW5kQGpteDRwZXJsLm9yZz6JAJUDBRNNcVaiQrIm
-k+8QEWUBARSrA/9gp7YhV7kh47WWtzC25aaW/WS2FwiBqKsOIJ5z8kkrEDOqz3iU
-TEzyHMgngwR7dNqZAM2xZlt6uTW1VuhraOFp27V0UVpQg/l1XaHF9JNVPvsbGmFG
-MIu/2gQrkhI9/Amyy5Zi3w2mbwISQ897QVY0O98/BlcymFpl5hrx4qbSdbQdUm9s
-YW5kIEh1c3MgPHJvbGFuZEBjcGFuLm9yZz6JAJUDBRA6aATCQrImk+8QEWUBAbKN
-A/9IEGDcSG7bB7ZW2oDzny++6nhpsHzRlSIwcXJA20W73bu/So8+v6fl4CiBEtZW
-KN6qCwqpreK6i8DHx+bGMkm8+uucO3G5vqi9FIF1yJt8ioLPyhPNktRGCCdSxbqG
-uYlOaDFwa9J9ebcqPe3mS0/374ixaArqpQPB+S/OU3nuXbQeUm9sYW5kIEh1c3Mg
-PHJvbGFuZEBjb25zb2wuZGU+iQCVAwUQOmgEQ0KyJpPvEBFlAQHI+AP9FbP3x5vs
-moXO95yV3PHhw0FOo9Szpd4kgIoXGMRVGC5gFKyX7dSU8jwi5PnSQRmTg8jQUUBj
-kVYi29nKHsOwp9J7oTbHlC02heaghjW5zTxxRv6lgmh3+cIsAimbi/fr3pRovRCT
-MS75CQJTAQAXz4+ALBxU3sG71kEx1mVwEIS0IFJvbGFuZCBIdXNzIDxyb2xhbmRA
-am9sb2tpYS5vcmc+iQCVAwUTTXFWgUKyJpPvEBFlAQHGcwP/UNWFVPiV+o3qWVfY
-+g9EiJoN43YN6QI3VasZ6Gjda3ZCJ6aLQXL9UorcTQBSIpCOKvEElG5Sw+dH0IPW
-jmrzWK1s9lnU2Qkx88QY5O489p+Z98SqbDGqW7DEIkYutYVou0nV7/SVyulMUNGe
-vqmY3GlfyqrXMXL+lu6IRpCfHcw=
-=HxAM
------END PGP PUBLIC KEY BLOCK-----
-
