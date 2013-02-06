fae832dfcc7602abdbfba8d6138b740bc2df1427
Roland Huß <roland@cpan.org>
2013-02-06 09:34:14 +0000

    Import of ROLAND/jmx4perl-1.07_1 from CPAN.
    gitpan-cpan-distribution: jmx4perl
    gitpan-cpan-version:      1.07_1
    gitpan-cpan-path:         ROLAND/jmx4perl-1.07_1.tar.gz
    gitpan-cpan-author:       ROLAND
    gitpan-cpan-maturity:     developer

--- lib/JMX/Jmx4Perl.pm
+++ lib/JMX/Jmx4Perl.pm
@@ -101,7 +101,7 @@ use vars qw($VERSION $HANDLER_BASE_PACKAGE @PRODUCT_HANDLER_ORDERING);
 use Data::Dumper;
 use Module::Find;
 
-$VERSION = "1.06";
+$VERSION = "1.07_1";
 
 my $REGISTRY = {
                 # Agent based
